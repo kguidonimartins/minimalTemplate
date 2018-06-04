08b7ce96d3a0e280ac883ed89d2a786d40c7a9ff
Karlo Guidoni Martins <kguidonimartins@gmail.com>
2018-06-04 13:03:00 -0300

    update wdiff
    simplify many things

--- R/setup_gitattributes.R
+++
@@ -1,9 +0,0 @@
-setup_gitattributes <- function(...) {
-  if (file.exists(".gitattributes")) {
-    print(".gitattributes exists!")
-  } else {
-    file.create(".gitattributes")
-    print(".gitattributes created")
-  }
-  write(x = "*.docx diff=pandoc", file = ".gitattributes")
-}
