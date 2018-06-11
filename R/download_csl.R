327f9f6ff6d025cf276f1ecfb0cf22af382ad9b8
Karlo Guidoni Martins <kguidonimartins@gmail.com>
2018-06-11 17:28:09 -0300

    typo

--- R/download_csl.R
+++ R/download_csl.R
@@ -1,6 +1,6 @@
 #' Download csl citation style
 #'
-#' Download csl citation style from \url{https://github.com/citation-style-language/styles}. 
+#' Download csl citation style from \url{https://github.com/citation-style-language/styles}.
 #'
 #' @param journal_style Journal style (see examples)
 #' @param directory Directory to store the csl file
@@ -9,14 +9,14 @@
 #' @export
 #'
 #' @examples
-#' # First, search by our prefered citation style using:
-#' eco <- grep(pattern = "ecology", x = rcrossref::get_styles(), value = TRUE)
+#' # First, search by your preferred citation style using:
+#' (eco <- grep(pattern = "ecology", x = rcrossref::get_styles(), value = TRUE))
 #' # then:
 #' download_csl(journal_style = eco[4], directory = "")
 download_csl <- function(journal_style, directory) {
-  
+
   style <- "https://raw.githubusercontent.com/citation-style-language/styles/master/"
-  
+
   download.file(paste0(style, journal_style, ".csl"), mode = "w",
                 destfile = paste0(directory, journal_style, ".csl"))
 }
