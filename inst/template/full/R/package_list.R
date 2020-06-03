8426704e0b0658185148cf3e650e33ab75867fde
Karlo Guidoni Martins <kguidonimartins@gmail.com>
2020-06-03 17:29:32 -0300

    package_list fills the DESCRIPTION file

--- inst/template/full/R/package_list.R
+++ inst/template/full/R/package_list.R
@@ -1,15 +1,27 @@
 # required; do not exclude from this list
 pkg_list <-
   c(
-    "devtools",
     "bibtex",
-    "knitr",
-    "rmarkdown",
-    "pacman",
+    "broom",
     "captioner",
-    "git2r"
+    "devtools",
+    "git2r",
+    "here",
+    "knitr",
+    "pacman",
+    "rmarkdown",
+    "tidyverse",
+    "usethis"
     )
 
 ipak(pkg_list)
 
 write.bib(pkg_list, here("manuscript", "sources", "installed-r-packages.bib"))
+
+suppressMessages({
+  pkg_list %>%
+    str_remove(., "tidyverse") %>% .[. != ""] %>%
+    map(~ use_package(package = .x, type = "Imports"))
+
+  use_package("tidyverse", type = "depends")
+})
